ca6fa30b8538cfc88db9783784b3ac16f61e76d7
JCzastor <153104971+JCzastor@users.noreply.github.com>
2023-12-07 09:04:51 +0100

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css';
+import './main.scss';
 import Countdown from "react-countdown/dist/LegacyCountdown";
 import StartButton from "./components/StartButton";
 import PauseButton from "./components/PauseButton";
 import Timer from "./components/Timer";
 import React, { useEffect, useRef, useState } from "react";
 import Settings from "./components/Settings";
-import SettingsContext from "./SettingsContext";
 
 function App() {
   return (
